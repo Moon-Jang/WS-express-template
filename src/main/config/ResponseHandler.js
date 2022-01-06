7cd701d096314591c7d1791e40fccea91b311d10
MoonJang <roby8502@naver.com>
2022-01-06 15:55:09 +0900

    ADD: cryptoUtil 추가

--- src/main/config/ResponseHandler.js
+++ src/main/config/ResponseHandler.js
@@ -1,5 +1,9 @@
 const ResponseHandler = {
     handle(req, res, next) {
+        if (!res.output) {
+            throw Error("잘못된 API입니다.")
+        }
+
         res.status(200)
         res.json(res.output)
     },
